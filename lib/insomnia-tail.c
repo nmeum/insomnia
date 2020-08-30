2e105873d61a313e098a09d69e7b47feb63e9f1d
Sören Tempel <soeren+git@soeren-tempel.net>
2020-08-30 17:29:34 +0200

    insomnia-tail: Handle SIGCHLD explicitly
    The waitpid call in the parent process is normally not invoked as
    readlines in not guaranteed to return. This may result in zombie
    processes. Additionally, exiting on tail-errors does not work properly
    without this change for the same reason.
    
    As an example consider in invocation such as:
    
    	$ insomnia-tail /non/existing/file

--- lib/insomnia-tail.c
+++ lib/insomnia-tail.c
@@ -1,6 +1,7 @@
 #include <err.h>
 #include <errno.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,6 +11,37 @@
 #include <sys/types.h>
 
 static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+static void
+sigchld(int num)
+{
+	pid_t r;
+	int status, wstatus;
+
+	(void)num;
+
+	status = EXIT_SUCCESS;
+	while ((r = waitpid((pid_t)-1, &wstatus, WNOHANG)) > 0) {
+		if (WIFEXITED(wstatus) && status == EXIT_SUCCESS)
+			status = WEXITSTATUS(wstatus);
+	}
+
+	if (status != EXIT_SUCCESS)
+		exit(status);
+}
+
+static void
+sethandler(void)
+{
+	struct sigaction act;
+
+	act.sa_flags = SA_RESTART;
+	act.sa_handler = sigchld;
+	if (sigemptyset(&act.sa_mask) == -1)
+		err(EXIT_FAILURE, "sigemptyset failed");
+	if (sigaction(SIGCHLD, &act, NULL))
+		err(EXIT_FAILURE, "sigaction failed");
+}
 
 /* thread-safe version of basename(dirname(fp)) */
 static ssize_t
@@ -76,7 +108,7 @@ tail(void *arg)
 {
 	pid_t pid;
 	FILE *stream;
-	int p[2], wstatus;
+	int p[2];
 	char *fp;
 
 	fp = arg;
@@ -95,12 +127,7 @@ tail(void *arg)
 	default:
 		if (!(stream = fdopen(p[0], "r")))
 		       err(EXIT_FAILURE, "fdopen failed");
-		readlines(fp, stream);
-
-		if (waitpid(pid, &wstatus, 0) == -1)
-			err(EXIT_FAILURE, "waitpid failed");
-		else if (WIFEXITED(wstatus))
-			exit(WEXITSTATUS(wstatus));
+		readlines(fp, stream); /* returns on stream EOF */
 
 		if (close(p[0]) || close(p[1]))
 			err(EXIT_FAILURE, "close failed");
@@ -124,6 +151,9 @@ main(int argc, char **argv)
 		fprintf(stderr, "USAGE: %s FILE...\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+
+	/* Setup SIGCHLD handler */
+	sethandler();
 
 	nthrs = argc - 1;
 	if (!(thrs = malloc(sizeof(pthread_t*) * nthrs)))
